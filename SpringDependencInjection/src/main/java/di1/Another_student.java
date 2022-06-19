242663982022bd5b206177b55a1b1b6c405fa800
Gagana Kumar <102527586+GaganaKumar@users.noreply.github.com>
2022-06-19 20:45:09 +0530

    first commit

---
+++ SpringDependencInjection/src/main/java/di1/Another_student.java
@@ -0,0 +1,16 @@
+package di1;
+
+public class Another_student {
+	//Dependency in objects
+	Cheat c;
+	//setters method for DI
+	public void setC(Cheat c) {
+		this.c = c;
+	}
+
+	public void cheating()
+	{
+		c.mathCheat2();
+	}
+			
+}
---
+++ SpringDependencInjection/src/main/java/di1/Student1.java
@@ -0,0 +1,15 @@
+package di1;
+
+public class Student1 {
+	Cheat c;
+	
+	
+	public void setC(Cheat c) {
+		this.c = c;
+	}
+	public void cheating() {
+		// TODO Auto-generated method stub
+		c.mathCheat2();
+	}
+
+}
